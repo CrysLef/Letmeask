d6498cb644b5ed67b6569a8aae4f495262a9164b
Crystian Lefundes <crystian.lf@gmail.com>
2021-09-18 10:36:56 -0300

    Create the Aside component

---
+++ src/components/AsideComponent/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+
+export const Aside = styled.aside`
+
+    flex: 7;
+
+    background-color: #835afd;
+    color: #FFF;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    padding: 102px 80px;
+
+    img {
+        min-width: 320px;
+    }
+
+    strong {
+        font: 700 36px 'Poppins';
+        line-height: 42px;
+        margin-top: 16px;
+    }
+
+    p {
+        font-size: 24px;
+        line-height: 32px;
+        margin-top: 16px;
+    }
+`
