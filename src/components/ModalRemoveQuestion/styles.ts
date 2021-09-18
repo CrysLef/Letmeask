c86e9cda847f70f59a6ad3ee6fd0efaca93598e5
Crystian Lefundes <crystian.lf@gmail.com>
2021-09-18 10:38:13 -0300

    Create the ModalRemoveQuestion component

---
+++ src/components/ModalRemoveQuestion/styles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components'
+
+
+
+export const Container = styled.div`
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    top: 0;
+    left: 0;
+    background-color: rgba(4, 9, 17, .8);
+    display: flex;
+`
+
+export const Modal = styled.div`
+    width: 590px;
+    background-color: #F8F8F8;
+    margin: auto;
+    border-radius: 8px;
+    font-family: 'Poppins',sans-serif;
+    text-align: center;
+    padding: 64px 0;
+        
+        svg {
+            width: 48px;
+            height: 48px;
+            
+            path {
+                stroke: #E73F5D;
+            }
+        } 
+        h2 {
+            font-size: 24px;
+            font-weight: 700;
+            margin-bottom: 12px;
+            color: #29292E;
+        }
+        
+        p {
+            margin-bottom: 24px;
+            color: #737380;
+        }
+
+`
+
+export const ButtonsWrapper = styled.div`
+
+    display: flex;
+    gap: 8px;
+    justify-content: center;
+    align-items: center;
+
+    button {
+        padding: 13px 32px;
+        border-radius: 8px;
+        border: 0;
+        background-color: #DBDCDD;
+        color: #737380;
+        cursor: pointer;
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.9);
+        }
+    }
+
+    .redButton {
+        background-color: #E73F5D;
+        color: #F8F8F8;
+    }
+`
