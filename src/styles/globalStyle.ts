cc8566a32dcfde98aa6bfca8e417728fb339fae4
Crystian Lefundes <crystian.lf@gmail.com>
2022-06-03 15:07:43 -0300

    Update global styles to stitches

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,35 +1,24 @@
-import { createGlobalStyle } from 'styled-components'
+import { globalCss } from 'styles/theme'
 
 
-export default createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+export const GlobalStyle = globalCss({
+    '*': {
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box',
+    },
 
-    html {
-        font-size: 62.5%;
-    }
+    'html': {
+        fontSize: '62.5%',
+    },
     
-    body {
-        background-color: ${props => props.theme.colors.background};
-        color: ${props => props.theme.colors.text};
+    'body': {
+        backgroundColor: '$background',
+        color: '$text',
+    },
+    
+   'body, input, button, textarea': {
+        fontWeight: 400,
+        fontFamily: '$roboto',
     }
-
-    body, input, button, textarea {
-        font-weight: 400;
-        font-family: 'Roboto', sans-serif;
-    }
-
-    .toggle {
-            position: fixed;
-            top: 0;
-            right: 0;
-            margin: 15px;
-
-            @media (max-width: 1400px) {
-                display: none;
-            }
-        }
-`
+})
