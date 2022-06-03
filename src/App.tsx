8130adec67a2da6f9d89c652866169e81584c7dc
Crystian Lefundes <crystian.lf@gmail.com>
2022-06-03 15:11:17 -0300

    Update Letmeask

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { AuthContextProvider } from './contexts/AuthContext';
+import { AppRoutes } from "routes";
+import { GlobalStyle } from "styles/globalStyle";
 
-import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
-import { Room } from './pages/Room';
-import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthContextProvider>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/rooms/new" component={NewRoom} />
-            <Route path="/rooms/:id" component={Room} />
-            <Route path="/admin/rooms/:id" component={AdminRoom} />
-        </Switch>
-      </AuthContextProvider>
-    </BrowserRouter>
+    <>
+      { GlobalStyle() }
+      <AppRoutes />
+    </>
   );
 }
 
